d273a6e0d224b950966e4b5d3d8bc2f9a5aa5c86
codingfray <olobuoy@gmail.com>
2023-04-08 11:17:20 +0100

    minor bug fix

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ let inputField = document.getElementById('inputField');
 addButton.addEventListener('click', function(){
     let paragraph = document.createElement('p');
     let deleteButton = document.createElement("button");
+    if (inputField.value === ""){
+        return;
+    }
     paragraph.classList.add('paragraph-styling');
     deleteButton.classList.add('paragraph-button');
     deleteButton.innerHTML = "Delete";
@@ -13,6 +16,7 @@ addButton.addEventListener('click', function(){
     addContainer.append(paragraph, deleteButton);
     // addContainer.appendChild(deleteButton);
     inputField.value = "";
+    
     paragraph.addEventListener('click', function(){
         paragraph.style.textDecoration = 'line-through';
         paragraph.style.color = 'grey';
